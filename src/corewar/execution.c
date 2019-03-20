3a8b91cac37a30ceaa5a1dc538d906209253ef8f
Pierre Carles <pcarles@student.42.fr>
2019-03-20 03:26:43 +0100

    Check ocp, program counter vaide pendant les operations

--- src/corewar/execution.c
+++ src/corewar/execution.c
@@ -6,7 +6,7 @@
 /*   By: pcarles <pcarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 14:06:58 by pcarles           #+#    #+#             */
-/*   Updated: 2019/03/13 18:07:58 by pcarles          ###   ########.fr       */
+/*   Updated: 2019/03/20 03:22:49 by pcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,42 +40,52 @@ static int		parse_ocp(t_op *op, uint8_t ocp, t_args *args)
 		mask >>= 2;
 		i++;
 	}
+	while (i < MAX_ARGS_NUMBER)
+	{
+		if ((ocp & mask) >> ((3 - i) * 2) != 0)
+			return (0);
+		mask >>= 2;
+		i++;
+	}
 	return (1);
 }
 
-static void		read_args(t_op *op, t_process *process, t_args *args, t_vm *vm)
+static uint16_t	read_args(t_op *op, t_process *process, t_args *args, t_vm *vm)
 {
 	uint8_t		ocp;
+	uint16_t	pc;
 	int			i;
 
 	i = 0;
+	pc = process->program_counter + 1;
 	if (op->ocp == 1) // If OCP we parse it and then we read arguments from memory
 	{
-		ocp = vm->memory[process->program_counter++];
-		process->program_counter %= MEM_SIZE;
+		ocp = read1_memory(vm, pc++);
 		if (parse_ocp(op, ocp, args) == 0)
 			crash(process, "bad ocp");
 		while (i < op->nb_params)
 		{
 			if (args->type[i] == e_reg)
 			{
-				args->value[i].u_reg = (int8_t)vm->memory[process->program_counter] - 1;
-				process->program_counter += 1; 
+				args->value[i].u_reg = read1_memory(vm, pc) - 1;
+				if (args->value[i].u_reg < 1 || args->value[i].u_reg >= REG_NUMBER)
+					crash(process, "invalid register");
+				pc += 1;
 			}
 			else if (args->type[i] == e_ind)
 			{
-				args->value[i].u_ind = read2_memory(vm, process->program_counter);
-				process->program_counter += 2;
+				args->value[i].u_ind = read2_memory(vm, pc);
+				pc += 2;
 			}
 			else if (args->type[i] == e_dir && op->little_dir == 1)
 			{
-				args->value[i].u_dir16 = read2_memory(vm, process->program_counter);
-				process->program_counter += 2;
+				args->value[i].u_dir16 = read2_memory(vm, pc);
+				pc += 2;
 			}
 			else if (args->type[i] == e_dir && op->little_dir == 0)
 			{
-				args->value[i].u_dir32 = read4_memory(vm, process->program_counter);
-				process->program_counter += 4;
+				args->value[i].u_dir32 = read4_memory(vm, pc);
+				pc += 4;
 			}
 			else
 				crash(process, "wtf this should never be reached, you can go hang yourself");
@@ -86,44 +96,47 @@ static void		read_args(t_op *op, t_process *process, t_args *args, t_vm *vm)
 	// special cases, operations with no OCP
 	else if (op->params[0] == T_DIR && op->little_dir == 1)
 	{
-		args->value[0].u_dir16 = read2_memory(vm, process->program_counter);
-		process->program_counter += 2;
+		args->value[0].u_dir16 = read2_memory(vm, pc);
+		pc += 2;
 	}
 	else if (op->params[0] == T_DIR && op->little_dir == 0)
 	{
-		args->value[0].u_dir32 = read4_memory(vm, process->program_counter);
-		process->program_counter += 4;
+		args->value[0].u_dir32 = read4_memory(vm, pc);
+		pc += 4;
 	}
 	else if (op->params[0] == T_REG)
 	{
-		args->value[0].u_reg = (int8_t)vm->memory[process->program_counter];
-		process->program_counter += 1;
+		args->value[0].u_reg = read1_memory(vm, pc);
+		pc += 1;
 	}
 	else
 		crash(process, "wtf this should never be reached, you can go hang yourself");
+	return (pc);
 }
 
 static void		do_op(t_process *process, t_vm *vm)
 {
 	uint8_t		op_code;
+	uint16_t	pc;
 	t_op		*op;
 	t_args		args;
 
-	op_code = vm->memory[process->program_counter++];
+	op_code = (uint8_t)read1_memory(vm, process->program_counter);
 	if (op_code < LIVE || op_code > AFF)
 	{
 		process->next_op = vm->cycle + 1;
+		process->program_counter++;
 		printf("Player (%s), bad opcode: 0x%.2x consumming 1 cycle\n", process->name, op_code);
 		return ;
 	}
 	op = &op_tab[op_code];
 	printf("Process %s doing op %s\n", process->name, op->name);
-	process->program_counter %= MEM_SIZE;
-	read_args(op, process, &args, vm);
+	pc = read_args(op, process, &args, vm);
 	if (op->func != NULL)
 		op->func(process, &args);
 	else
 		printf("No func for operation %s :(\n", op->name);
+	process->program_counter = pc;
 	process->next_op = vm->cycle + op->cycles;
 }
 
@@ -134,7 +147,7 @@ void			launch(t_vm *vm)
 	while (42)
 	{
 //		printf("cycle: %zu\n", vm->cycle);
-		if (vm->cycle == 100)
+		if (vm->cycle == 500)
 			break ;
 		i = 0;
 		while (i < vm->nb_champs)
--- src/corewar/op.c
+++ src/corewar/op.c
@@ -6,7 +6,7 @@
 /*   By: pcarles <pcarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 14:21:51 by pcarles           #+#    #+#             */
-/*   Updated: 2019/03/13 18:09:02 by pcarles          ###   ########.fr       */
+/*   Updated: 2019/03/20 03:20:38 by pcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,92 +15,21 @@
 
 #include "corewar.h"
 
-int32_t			read4_memory(t_vm *vm, size_t index)
+static void	get_value_of_arg(t_process *process, t_int_types *value, t_int_types_enum *type) // ajouter t_op pour dir16/32 et indirect valeur ou address
 {
-	int32_t		*tmp;
-	uint8_t		tab[4];
-	size_t		i;
-
-	index %= MEM_SIZE;
-	if (MEM_SIZE - index > 4)
-		tmp = (int32_t*)&vm->memory[index];
-	else
+	if (*type == e_reg) 
 	{
-		tmp = (int32_t*)tab;
-		i = 0;
-		ft_bzero(tab, sizeof(tab));
-		while (i < 4)
-		{
-			tab[i] = vm->memory[index];
-			index++;
-			i++;
-			index %= MEM_SIZE;
-		}
+		(*value).u_dir32 = process->registers[(*value).u_reg];
+		*type = e_dir;
 	}
-	return (swap_int32(*tmp));
-}
-
-int16_t			read2_memory(t_vm *vm, size_t index)
-{
-	int16_t		*tmp;
-	uint8_t		tab[2];
-	size_t		i;
-
-	index %= MEM_SIZE;
-	if (MEM_SIZE - index > 2)
-		tmp = (int16_t*)&vm->memory[index];
-	else
+	else if (*type == e_ind)
 	{
-		tmp = (int16_t*)tab;
-		i = 0;
-		ft_bzero(tab, sizeof(tab));
-		while (i < 2)
-		{
-			tab[i] = vm->memory[index];
-			index++;
-			i++;
-			index %= MEM_SIZE;
-		}
-	}
-	return (swap_int16(*tmp));
-}
-
-void		write4_memory(t_vm *vm, int32_t value, size_t index)
-{
-	int32_t		*tmp;
-	uint8_t		tab[4];
-	size_t		i;
-
-	tmp = (int32_t*)tab;
-	*tmp = swap_int32(value);
-	i = 0;
-	while (i < 4)
-	{
-		index %= MEM_SIZE;
-		vm->memory[index] = tab[i];
-		index++;
-		i++;
+		(*value).u_dir32 = read4_memory(get_vm(NULL), process->program_counter + ((*value).u_ind) % IDX_MOD);
+		*type = e_dir;
 	}
 }
 
-//jac 12032019
-void		write2_memory(t_vm *vm, int16_t value, size_t index)
-{
-	uint8_t		tab[2];
-	size_t		i;
-
-	*((int16_t*)tab) = swap_int16(value);
-	i = 0;
-	while (i < 2)
-	{
-		index %= MEM_SIZE;
-		vm->memory[index] = tab[i];
-		index++;
-		i++;
-	}
-}
-
-void		op_live(t_process *process, t_args *args)
+void		op_live(t_process *process, t_args *args) // OK
 {
 	int32_t	arg;
 	t_vm	*vm;
@@ -109,23 +38,37 @@ void		op_live(t_process *process, t_args *args)
 	vm = get_vm(NULL);
 	arg = args->value[0].u_dir32;
 	if (arg > 0 && arg <= (int)vm->nb_champs)
+	{
 		vm->process[arg - 1].live_counter++;
-	printf("player %d is alive\n", arg);
+		printf("player %d is alive\n", arg);
+	}
+	else
+		printf("unknown player %d is alive\n", arg);
 }
 
-void		op_zjmp(t_process *process, t_args *args)
+void		op_ld(t_process *process, t_args *args) // OK
 {
-	if (process->carry == 1)
-		process->program_counter += args->value[0].u_dir16 - 3;
+	int32_t	result;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0], LD);
+	result = args->value[0].u_dir32;
+	process->registers[args->value[1].u_reg] = result;
+	process->carry = (result == 0) ? 1 : 0;
 }
 
-void		op_aff(t_process *process, t_args *args)
+void		op_st(t_process *process, t_args *args) // OK
 {
-	printf("Process %s is saying `%c'\n", process->name, \
-	process->registers[args->value[0].u_reg] % 256);
+	int32_t	value_to_store;
+
+	value_to_store = process->registers[args->value[0].u_reg];
+	if (args->type[1] == e_ind)
+		write4_memory(get_vm(NULL), value_to_store, process->program_counter + (args->value[1].u_ind % IDX_MOD));
+	else if (args->type[1] == e_reg)
+		process->registers[args->value[1].u_reg] = value_to_store;
+	process->carry = (value_to_store == 0) ? 1 : 0;
 }
 
-void		op_add(t_process *process, t_args *args)
+void		op_add(t_process *process, t_args *args) // OK
 {
 	int32_t	result;
 
@@ -135,7 +78,7 @@ void		op_add(t_process *process, t_args *args)
 	process->carry = (result == 0) ? 1 : 0;
 }
 
-void		op_sub(t_process *process, t_args *args)
+void		op_sub(t_process *process, t_args *args) // OK
 {
 	int32_t	result;
 
@@ -143,4 +86,161 @@ void		op_sub(t_process *process, t_args *args)
 		- process->registers[args->value[1].u_reg];
 	process->registers[args->value[2].u_reg] = result;
 	process->carry = (result == 0) ? 1 : 0;
+}
+
+void		op_and(t_process *process, t_args *args) // OK
+{
+	int32_t	result;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+	result = args->value[0].u_dir32 & args->value[1].u_dir32;
+	process->registers[args->value[2].u_reg] = result;
+	process->carry = (result == 0) ? 1 : 0;
+	printf("AND: %x & %x = %x\n", args->value[0].u_dir32, args->value[1].u_dir32, result);
+}
+
+void		op_or(t_process *process, t_args *args) // OK
+{
+	int32_t	result;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+	result = args->value[0].u_dir32 | args->value[1].u_dir32;
+	process->registers[args->value[2].u_reg] = result;
+	process->carry = (result == 0) ? 1 : 0;
+	printf(" OR: %x | %x = %x\n", args->value[0].u_dir32, args->value[1].u_dir32, result);
+}
+
+void		op_xor(t_process *process, t_args *args) // OK
+{
+	int32_t	result;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+	result = args->value[0].u_dir32 ^ args->value[1].u_dir32;
+	process->registers[args->value[2].u_reg] = result;
+	process->carry = (result == 0) ? 1 : 0;
+	printf("XOR: %x ^ %x = %x\n", args->value[0].u_dir32, args->value[1].u_dir32, result);
+}
+
+void		op_zjmp(t_process *process, t_args *args) // OK
+{
+	if (process->carry == 1)
+		process->program_counter += args->value[0].u_dir16;
+}
+/* OLD
+int32_t		if_registre(int32_t value, t_process *process, int byte)
+{
+	t_vm *vm;
+
+	vm = get_vm(NULL);
+	value = ((vm->memory[(process->program_counter + 1) % MEM_SIZE] >> byte & 0x03) == 1) ?
+		process->registers[value] : value;
+	return (value);
+}
+*/
+void		op_ldi(t_process *process, t_args *args)
+{
+	int32_t	value;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+//	address = process->program_counter + ((args->value[0].u_dir32 + args->value[1].u_dir32) % IDX_MOD); // ou (all % IDX_MOD)
+//	process->registers[args->value[2].u_reg] = read4_memory(get_vm(NULL), address); // pas write plutot?
+	args->value[0].u_dir32 = (args->type[0] == e_reg) ? 
+		process->registers[args->value[0].u_reg] : args->value[0].u_dir32; // atention le arg0 peut etre un indirect
+	args->value[1].u_dir32 = (args->type[1] == e_reg) ?
+		process->registers[args->value[1].u_reg] : args->value[1].u_dir32;
+	value = (args->value[0].u_dir32 + args->value[1].u_dir32) % IDX_MOD; // j ai mis u_dir32 a chaque pour eviter les erreurs de compil a voir si c est juste
+	process->registers[args->value[2].u_reg] = value;
+	process->carry = (process->registers[args->value[2].u_reg] == 0) ? 1 : 0; // ou value == 0
+}
+
+void		op_sti(t_process *process, t_args *args)
+{
+	int32_t	value_to_store; // est ce que ca serait un int8_t?
+	int32_t address;
+
+	value_to_store = process->registers[args->value[0].u_reg];
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+	get_value_of_arg(process, &args->value[2], &args->type[2]);
+	args->value[1].u_dir32 = (args->type[1] == e_reg) ?
+		process->registers[args->value[1].u_reg] : args->value[1].u_dir32; // atention arg1 peut etre un indirect
+	args->value[2].u_dir32 = (args->type[2] == e_reg) ?
+		process->registers[args->value[2].u_reg] : args->value[2].u_dir32;
+	address = (args->value[1].u_dir32 + args->value[2].u_dir32) % IDX_MOD; // j ai mis u_dir32 a chaque pour eviter les erreurs de compil a voir si c est juste 
+	write4_memory(get_vm(NULL), value_to_store, process->program_counter + address);
+	process->carry = (value_to_store == 0) ? 1 : 0;
+}
+
+void		op_aff(t_process *process, t_args *args)
+{
+	printf("Process %s is saying `%c'\n", process->name, \
+	process->registers[args->value[0].u_reg] % 256);
+}
+
+void		op_lld(t_process *process, t_args *args)
+{
+	int32_t	result;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	result = args->value[0].u_dir32;
+	process->registers[args->value[1].u_reg] = result;
+	process->carry = (result == 0) ? 1 : 0;
+}
+
+void		op_lldi(t_process *process, t_args *args)
+{
+	int32_t	value;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	get_value_of_arg(process, &args->value[1], &args->type[1]);
+	args->value[0].u_dir32 = (args->type[0] == e_reg) ?
+		process->registers[args->value[0].u_reg] : args->value[0].u_dir32;  // atention arg0 peut etre un indirect
+	args->value[1].u_dir32 = (args->type[1] == e_reg) ?
+		process->registers[args->value[1].u_reg] : args->value[1].u_dir32;
+	value = (args->value[0].u_dir32 + args->value[1].u_dir32);
+	process->registers[args->value[2].u_reg] = value;
+	process->carry = (process->registers[args->value[2].u_reg] == 0) ? 1 : 0; // ou value == 0
+}
+
+t_process	*ft_copy_process(t_process *process)
+{
+	t_process	*new_process;
+	t_vm		*vm;
+
+	vm = get_vm(NULL);
+	if (!(new_process = (t_process *)ft_memalloc(sizeof(t_process))))
+	{
+		printf("erreur malloc copy process dans fork");
+		return (NULL);
+	}
+	ft_memcpy(new_process, process, sizeof(t_process)); // faire un parcours des next jusqu a null pour copy le next;
+	process->next = new_process; // il faut bien ajouter un process mais je ne sais pas si c est la bonne maniere de faire
+	return (new_process);
+
+}
+
+void		op_fork(t_process *process, t_args *args)
+{
+	t_process 	*new_process;
+	
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	args->value[0].u_dir32 %= IDX_MOD;
+	new_process = ft_copy_process(process);
+	new_process->program_counter = (process->program_counter + args->value[0].u_dir32) % MEM_SIZE;
+	if (new_process->program_counter < 0)
+		new_process->program_counter += MEM_SIZE;
+}
+
+void		op_lfork(t_process *process, t_args *args)
+{
+	t_process *new_process;
+
+	get_value_of_arg(process, &args->value[0], &args->type[0]);
+	new_process = ft_copy_process(process);
+	new_process->program_counter = (process->program_counter + args->value[0].u_dir32) % MEM_SIZE;
+	if (new_process->program_counter < 0)
+		new_process->program_counter += MEM_SIZE;
 }
