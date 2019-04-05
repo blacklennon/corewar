f0918c27885237780e4506e7504bd0057b3be308
Pierre Carles <pcarles@student.42.fr>
2019-04-05 20:32:21 +0200

    Add color to anounce_winner function

--- src/corewar/cw_00_main.c
+++ src/corewar/cw_00_main.c
@@ -6,7 +6,7 @@
 /*   By: pcarles <pcarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/26 14:38:49 by pcarles           #+#    #+#             */
-/*   Updated: 2019/04/03 18:07:29 by pcarles          ###   ########.fr       */
+/*   Updated: 2019/04/04 16:50:32 by pcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,9 +34,12 @@ static void	set_nb_champs(t_vm *vm)
 
 static void	anounce_winner(t_vm *vm)
 {
-	if (vm->last_alive != NULL)
-		ft_printf("And the winner is champion %d, %s\n", \
-		vm->last_alive->id, vm->last_alive->name);
+	t_champion	*winner;
+
+	winner = vm->last_alive;
+	if (winner != NULL)
+		ft_printf("\e[%dmAnd the winner is champion %d, %s\e[0m\n", \
+		31 + winner->id, winner->id, winner->name);
 	else
 		ft_printf("Nobody wins\n");
 	ft_printf("Game ended at cylce %d\n", vm->cycle);
@@ -57,7 +60,8 @@ int			main(int ac, char **av)
 	launch(&vm);
 	if (vm.cycle_limit != 0)
 		mem_dump(vm.memory, MEM_SIZE, MEM_SIZE + 1);
-	anounce_winner(&vm);
+	else
+		anounce_winner(&vm);
 	free_process(vm.process);
 	return (EXIT_SUCCESS);
 }
