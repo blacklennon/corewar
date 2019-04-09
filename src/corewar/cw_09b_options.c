3a27087433832d836068e8ec603c63188624c3dc
Pierre Carles <pcarles@student.42.fr>
2019-04-09 19:02:22 +0200

    For the norm

--- src/corewar/cw_09b_options.c
+++ src/corewar/cw_09b_options.c
@@ -6,7 +6,7 @@
 /*   By: pcarles <pcarles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 18:42:57 by pcarles           #+#    #+#             */
-/*   Updated: 2019/04/09 18:50:08 by pcarles          ###   ########.fr       */
+/*   Updated: 2019/04/09 19:02:11 by pcarles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static void	add_player(char *file_path, t_vm *vm)
 	crash(NULL, "too many players");
 }
 
-void			try_add_player(char **av, int i, t_vm *vm)
+void		try_add_player(char **av, int i, t_vm *vm)
 {
 	if (ft_strstr(av[i], ".cor") != NULL)
 		add_player(av[i], vm);
